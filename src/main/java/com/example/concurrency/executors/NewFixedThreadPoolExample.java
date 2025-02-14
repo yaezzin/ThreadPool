30a7475e0ecd9755afeed9e9e4b2775e7f96defb
yaezzin <yaezzin@gmail.com>
2025-02-15 01:29:08 +0900

    Executors Class NewFixedThreadPool

---
+++ src/main/java/com/example/concurrency/executors/NewFixedThreadPoolExample.java
@@ -0,0 +1,18 @@
+package com.example.concurrency.executors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class NewFixedThreadPoolExample {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 10; i++) {
+            Runnable task = () -> {
+                System.out.println("Task executed by: " + Thread.currentThread().getName());
+            };
+            executorService.execute(task);
+        }
+        executorService.shutdown();
+    }
+}
