4015182900ade2e9b3bd0397b7abf0a21752cca3
yaezzin <yaezzin@gmail.com>
2025-02-20 17:11:48 +0900

    Add lombok

---
+++ src/main/java/com/example/concurrency/executor/ThreadPoolTaskExecutorConfig.java
@@ -0,0 +1,21 @@
+package com.example.concurrency.executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class ThreadPoolTaskExecutorConfig {
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        return executor;
+    }
+
+}
